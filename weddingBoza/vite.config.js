230a033d8e036e02a90ddb1b0d9afb2e652bc053
santiago alvarez <santiagoalvarez290401@gmail.com>
2024-08-21 22:26:51 -0300

    modify viteconfig to fix router error

--- weddingBoza/vite.config.js
+++ weddingBoza/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,  // Redirige todas las solicitudes al index.html
+  }
 })
